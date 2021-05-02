bca00f109c58f948def369de85b0d6e97ff0b213
trykA <claudiu.tancau08@gmail.com>
2021-05-02 18:01:42 +0300

    Created components for music Player and Playlist

---
+++ frontend/src/components/Playlist.js
@@ -0,0 +1,12 @@
+const Playlist = () => {
+    return (
+        <div className='playlist'>
+            <input type='text' placeholder='Search'></input>
+            <div className='tracklist'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Playlist
